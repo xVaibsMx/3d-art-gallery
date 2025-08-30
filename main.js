422a5710888d65396a9fd8a6b72f1c75b4a91131
MxVaibs <mvaibhav.main@gmail.com>
2025-08-30 20:59:44 +0530

    Added images and enabled 3d rotation.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 
+// Constants Setup
 const images = [
   '/AOT.jpg',
   '/Spiderman.jpg',
@@ -18,20 +19,21 @@ const names = [
   'Son Goku',
 ]
 
+// Texture Loader Setup
+const textureLoader = new THREE.TextureLoader()
+
 // Renderer Setup
 const renderer = new THREE.WebGLRenderer({ antialias: true })
-const w = window.innerWidth
-const h = window.innerHeight
-renderer.setSize(w, h)
+renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 // Camera Setup
-const fov = 75
-const aspect = w / h
-const near = 0.1
-const far = 1000
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.z = -5
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
 
 // Scene Setup
 const scene = new THREE.Scene()
@@ -39,12 +41,36 @@ const scene = new THREE.Scene()
 const rootNode = new THREE.Object3D()
 scene.add(rootNode)
 
-const baseNode = new THREE.Object3D()
-rootNode.add(baseNode)
+let count = 6
 
-// Function Call
+for (let i = 0; i < count; i++) {
+  const texture = textureLoader.load(images[i])
+  texture.colorSpace = THREE.SRGBColorSpace;
+  const baseNode = new THREE.Object3D()
+  baseNode.rotation.y = i * ((2 * Math.PI) / 6)
+  rootNode.add(baseNode)
+
+  const artGeo = new THREE.BoxGeometry(3, 2, 0.1)
+  const artMat = new THREE.MeshBasicMaterial({ map: texture })
+  const artworks = new THREE.Mesh(artGeo, artMat)
+  artworks.position.z = -4
+  baseNode.add(artworks)
+}
+
+// Animation Loop
 function animate() {
+  rootNode.rotation.y += 0.005
+  requestAnimationFrame(animate)
   renderer.render(scene, camera)
 }
+
+// Resize Handler
+window.addEventListener('resize', () => {
+  const w = window.innerWidth
+  const h = window.innerHeight
+  camera.aspect = w / h
+  camera.updateProjectionMatrix()
+  renderer.setSize(w, h)
+})
 
 animate()
