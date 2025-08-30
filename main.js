ec61abff74e68fee699a0eb58d0b50ea3f4dfa45
MxVaibs <mvaibhav.main@gmail.com>
2025-08-30 11:55:27 +0530

    added constants.

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 import * as THREE from 'three'
+
+const images = [
+  '/AOT.jpg',
+  '/Spiderman.jpg',
+  '/KakashiHatake.jpg',
+  '/Luffy.jpg',
+  '/Obanai.jpg',
+  '/Goku.jpg',
+]
+
+const names = [
+  'Attack On Titan',
+  'Spiderman',
+  'Kakashi Hatake',
+  'Monkey D Luffy',
+  'Iguru Obanai',
+  'Son Goku',
+]
